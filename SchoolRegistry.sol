3e8818fbd170751524ab2b72aafeb4b1d46b39e9
SatyoshiNakamario <kijekpat@gmail.com>
2018-05-04 20:31:35 -0500

    Updated Webpage

--- SchoolRegistry.sol
+++ SchoolRegistry.sol
@@ -2,28 +2,28 @@ pragma solidity ^0.4.15;
 import "./School.sol";
 
 contract SchoolRegistry{
-    
+
     address registryOwner;
-    
+
     struct school{
         bytes32 schoolName;
         address schoolContract;
     }
-    
+
     school[] registeredSchools;
     mapping(address => bool) inRegistry;
-    
+
     function SchoolRegistry() public{
         registryOwner = msg.sender;
     }
-    
+
     modifier onlyRegistryOwner(){
         if (msg.sender != registryOwner){
             revert();
         }
         _;
     }
-    
+
     function addSchoolToRegistry(bytes32 _schoolName) onlyRegistryOwner public{
         address newSchool = new School(_schoolName);
         if (newSchool == 0x0) {revert();}
@@ -33,7 +33,7 @@ contract SchoolRegistry{
         }));
         inRegistry[newSchool] = true;
     }
-    
+
     function schoolInRegistry(address _school) constant public returns(bool){
         inRegistry[_school];
     }
